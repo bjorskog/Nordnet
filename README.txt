4fe576e7d08b462d6e275866c7b0a80c51104454
bjorskog <bjorn.skogtro@gmail.com>
2012-06-26 00:31:25 +0200

    Initial commit - only package structure.
    Skeleton of the package.

---
+++ README.txt
@@ -0,0 +1,7 @@
+===========
+Nordnet API
+===========
+
+A package for connecting to the Nordnet API.
+
+
---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/en python
+
+from setuptools import setup
+
+setup(
+    name='Nordnet',
+    version='0.1.0',
+    author='bjorskog',
+    author_email='bjorn.skogtro@gmail.com',
+    packages=['nordnet', 'nordnet.tests'],
+    entry_points={
+        'console_scripts':[],
+        },
+    test_suite='nose.collector',
+    tests_require='nose',
+    license='LICENSE.txt',
+    description='A Python package for connecting to Nordnets API.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "nose >= 1.1.2"
+    ],)
+    
